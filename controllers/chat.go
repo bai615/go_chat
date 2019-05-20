c020c1a0d15251abb8e6314eb0449023f0a1d304
baihua <baihua_2011@163.com>
2019-05-20 11:06:35 +0800

    实现发送文字、表情包

--- controllers/chat.go
+++ controllers/chat.go
@@ -8,7 +8,27 @@ import (
 	"log"
 	"fmt"
 	"strconv"
+	"encoding/json"
 )
+
+const (
+	CMD_SINGLE_MSG = 10
+	CMD_ROOM_MSG   = 11
+	CMD_HEART      = 0
+)
+
+type Message struct {
+	Id      int64  `json:"id,omitempty" form:"id"`           // 消息ID
+	Userid  int64  `json:"userid,omitempty" form:"userid"`   // 谁发的
+	Cmd     int    `json:"cmd,omitempty" form:"cmd"`         // 群聊还是私聊
+	Dstid   int64  `json:"dstid,omitempty" form:"dstid"`     // 对端用户ID/群ID
+	Media   int    `json:"media,omitempty" form:"media"`     // 消息按照什么样式展示
+	Content string `json:"content,omitempty" form:"content"` // 消息的内容
+	Pic     string `json:"pic,omitempty" form:"pic"`         // 预览图片
+	Url     string `json:"url,omitempty" form:"url"`         // 服务的URL
+	Memo    string `json:"memo,omitempty" form:"memo"`       // 简单描述
+	Amount  int    `json:"amount,omitempty" form:"amount"`   // 其他和数字相关的
+}
 
 type Node struct {
 	Conn *websocket.Conn
@@ -50,7 +70,7 @@ func Chat(writer http.ResponseWriter, request *http.Request) {
 
 	// todo 获得 conn
 	node := &Node{
-		Conn: conn,
+		Conn:      conn,
 		DataQueue: make(chan []byte, 50),
 		GroupSets: set.New(set.ThreadSafe),
 	}
@@ -92,7 +112,28 @@ func recvproc(node *Node) {
 			return
 		}
 		// todo 对data进一步处理
+		dispatch(data)
 		fmt.Printf("recv<=%s", data)
+	}
+}
+
+// 后端调度逻辑处理
+func dispatch(data []byte) {
+	// todo 解析 data 为 message
+	msg := Message{}
+	err := json.Unmarshal(data, &msg)
+	if nil != err {
+		log.Println(err.Error())
+		return
+	}
+	// todo 根据 cmd 对逻辑进行处理
+	switch msg.Cmd {
+	case CMD_SINGLE_MSG: // 单聊
+		sendMsg(msg.Dstid, data)
+	case CMD_ROOM_MSG: // 群聊
+		// todo 群聊转发逻辑
+	case CMD_HEART: // 心跳
+		// todo 一般啥也不做
 	}
 }
 
