ab8dede53dd82f7669b819ae753eb48250683c3b
baihua <baihua_2011@163.com>
2019-05-20 10:29:00 +0800

    socket 接入

--- controllers/chat.go
+++ controllers/chat.go
@@ -7,11 +7,12 @@ import (
 	"sync"
 	"log"
 	"fmt"
+	"strconv"
 )
 
 type Node struct {
 	Conn *websocket.Conn
-	// 并行转串行,
+	// 并行转串行, Conn 为 IO 型资源，有忙和闲的概念，存在竞争关系。一个 Conn 正在写数据时，另一个 Conn 接入数据会混乱。并行转串行后，数据为顺序型。
 	DataQueue chan []byte
 	GroupSets set.Interface
 }
@@ -26,22 +27,46 @@ var rwlocker sync.RWMutex
 func Chat(writer http.ResponseWriter, request *http.Request) {
 
 	// todo 检验接入是否合法
-	// checkToken
+	// checkToken(userId int64, token string)
+	query := request.URL.Query() // 获取 URL 中参数
+	id := query.Get("id")
+	token := query.Get("token")
+	// 将字符串转为 int64 型
+	userId, _ := strconv.ParseInt(id, 10, 64)
+	isvalida := checkToken(userId, token)
+	// 如果 isvalida = true 继续
+	// isvalida = false 返回
+
+	// 自动升级
+	conn, err := (&websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return isvalida
+		},
+	}).Upgrade(writer, request, nil)
+	if nil != err {
+		log.Println(err.Error())
+		return
+	}
 
 	// todo 获得 conn
 	node := &Node{
+		Conn: conn,
 		DataQueue: make(chan []byte, 50),
 		GroupSets: set.New(set.ThreadSafe),
 	}
 
 	// todo userid 和 node 形成绑定关系
+	rwlocker.Lock()
+	clientMap[userId] = node
+	rwlocker.Unlock()
 
 	// todo 完成发送逻辑，con
 	go sendproc(node)
 	// todo 完成接收逻辑
 	go recvproc(node)
 
-	// sendMsg(userId, []byte("hello, world!"))
+	// 发送信息
+	sendMsg(userId, []byte("hello, world!"))
 }
 
 // 发送协程
